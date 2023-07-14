9ce04aeda4fd6ec6b8343298abfcf6163123f0c2
Sanan <116113098+SananIsmayilov@users.noreply.github.com>
2023-07-14 10:46:14 +0400

    UserOrder class added

--- app/src/main/java/com/example/sananismayilov/myprojectsale/İntentAcivity/UserOrder.java
+++ app/src/main/java/com/example/sananismayilov/myprojectsale/İntentAcivity/UserOrder.java
@@ -1,16 +1,19 @@
 package com.example.sananismayilov.myprojectsale.İntentAcivity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.example.sananismayilov.myprojectsale.R;
 
 public class UserOrder extends AppCompatActivity {
-
+RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_order);
+        recyclerView = findViewById(R.id.recylerviewtoorders);
+
     }
 }
