67fc18e0d6afa95ff80e7362544c8e3bd034f33a
Sanan <116113098+SananIsmayilov@users.noreply.github.com>
2023-07-14 17:07:44 +0400

    AdapterToOrders added

---
+++ app/src/main/java/com/example/sananismayilov/myprojectsale/Adapters/AdapterToOrders.java
@@ -0,0 +1,58 @@
+package com.example.sananismayilov.myprojectsale.Adapters;
+
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.sananismayilov.myprojectsale.databinding.ViewforordersBinding;
+
+import java.util.ArrayList;
+
+public class AdapterToOrders extends RecyclerView.Adapter<AdapterToOrders.Ordersholder> {
+    ArrayList<ConteynerToOrders> orders;
+
+    public AdapterToOrders(ArrayList<ConteynerToOrders> orders) {
+        this.orders = orders;
+    }
+
+    @NonNull
+    @Override
+    public Ordersholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ViewforordersBinding binding = ViewforordersBinding.inflate(LayoutInflater.from(parent.getContext()),parent,false);
+        return new Ordersholder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Ordersholder holder, int position) {
+        holder.binding.ordertoname.setText(orders.get(position).getProduct_name());
+        holder.binding.ordertomodel.setText(orders.get(position).getProduct_model());
+        String status = orders.get(position).getStatus();
+
+        if(status.equals("1")){
+            holder.binding.ordertoname.setTextColor(Color.GREEN);
+            holder.binding.ordertomodel.setTextColor(Color.GREEN);
+        } else {
+            holder.binding.ordertoname.setTextColor(Color.BLACK);
+            holder.binding.ordertomodel.setTextColor(Color.BLACK);
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return orders.size();
+    }
+
+    public class Ordersholder extends RecyclerView.ViewHolder{
+        ViewforordersBinding binding;
+
+        public Ordersholder(@NonNull  ViewforordersBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+}
